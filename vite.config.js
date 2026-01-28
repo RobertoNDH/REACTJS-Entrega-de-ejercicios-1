f32a10a4ab3652ee87c8b42d21a92bfb724e0d68
RobertoNDH <robertonauzet@gmail.com>
2026-01-28 12:37:50 +0000

    feat: initialize project with vite and professional mpa architecture

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/REACTJS-Entrega-de-ejercicios-1/",
+  build: {
+  rollupOptions: {
+  input: {
+  main: resolve(__dirname, "index.html"),
+  ejercicio1: resolve(__dirname, "react_ejercicio_1.html"),
+  ejercicio2: resolve(__dirname, "react_ejercicio_2.html"),
+  ejercicio3: resolve(__dirname, "react_ejercicio_3.html"),
+  ejercicio4: resolve(__dirname, "react_ejercicio_4.html"),
+  ejercicio5: resolve(__dirname, "react_ejercicio_5.html"),
+  ejercicio6: resolve(__dirname, "react_ejercicio_6.html"),
+  ejercicio7: resolve(__dirname, "react_ejercicio_7.html"),
+  ejercicio8: resolve(__dirname, "react_ejercicio_8.html"),
+  ejercicio9: resolve(__dirname, "react_ejercicio_9.html"),
+  }
+  }
+  }
 })
