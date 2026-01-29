1806e32c563609d88b8fe1e8768b553bc2c30660
RobertoNDH <robertonauzet@gmail.com>
2026-01-29 22:15:42 +0000

    feat: add main entry point for React application

---
+++ src/main_ejercicio1.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Ejercicio1 from './React_ejercicio1'; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ejercicio1 />
+  </React.StrictMode>
+);
