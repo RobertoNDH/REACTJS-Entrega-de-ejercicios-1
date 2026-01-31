58be7d090386b0a378b9caa121a233af46606e43
RobertoNDH <robertonauzet@gmail.com>
2026-01-31 23:29:57 +0000

    feat: add main entry points for Ejercicio2 and Ejercicio3 components

---
+++ src/main_ejercicio2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Ejercicio2 from './react_ejercicio2'; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ejercicio2 />
+  </React.StrictMode>
+);
---
+++ src/main_ejercicio3.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Ejercicio3 from './react_ejercicio3'; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ejercicio3 />
+  </React.StrictMode>
+);
