c99e18c89318c9975e750c3b1775a1db72cac7bd
RobertoNDH <robertonauzet@gmail.com>
2026-02-02 11:22:19 +0000

    feat: add main rendering logic for Ejercicio 9 component

---
+++ src/main_ejercicio9.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Ejercicio9 from './react_ejercicio9'; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Ejercicio9 />
+  </React.StrictMode>
+);
